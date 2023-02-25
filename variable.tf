f0e855ec3cba9f56a0ae1c233dbe51b60ec7c838
Karthika M <123933555+karthik1234567899@users.noreply.github.com>
2023-02-25 20:14:59 +0530

    Add files via upload
    new files uploaded

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "AKIA6RPJFY3HNB74WTGP"
+}
+variable "aws_secret_key" {
+default = "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"
+ }
+variable "region" {
+    default = "region"
+}
